952cebb6691536450fa8cdafc02518fbcb0e085e
jherson <rodriguezpaul98@gmail.com>
2025-05-08 00:03:48 -0400

    change env name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   backendApi: 'https://construction-budget-back.onrender.com/gpt',
-  assistantApi: 'https://construction-budget-back.onrender.com/assistant-event',
+  assistantApi: 'https://construction-budget-back.onrender.com/event',
 };
